3b21657161c917351f35360d951eaa8336b16888
Max DePriest <maxd924@gmail.com>
2015-07-17 17:02:44 -0500

    adding click events

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 $(document).ready(function() {
 	// Listen for floor class div click and change color.
 	// If clicked, change this and keep others same color
-	$('.floor').
+	$('.floor').on('click', function() {
+		$('.floor').css('background-color', 'blue');
+		$(this).css('background-color', 'yellow');
+	});
+
+	$('.level').on('click', function() {
+		$('.level').css('background-color', 'red');
+		$(this).css('background-color', 'blue');
+	});
+
+	$('#clear').on('click', function() {
+		$('.level').css('background-color', 'red');
+		$('.floor').css('background-color', 'blue');
+	});
+
+
 
 	// Listen for level class div click and change color.
 	// If clicked, change this and keep others same color
