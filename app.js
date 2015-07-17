f74bd45f414093861a2420e031b9b9f39e750ca6
Max DePriest <maxd924@gmail.com>
2015-07-17 16:47:20 -0500

    pseudocoding

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-$('')
+$(document).ready(function() {
+	// Listen for floor class div click and change color.
+	// If clicked, change this and keep others same color
+	$('.floor').
+
+	// Listen for level class div click and change color.
+	// If clicked, change this and keep others same color
+
+	// listen for save button click
+
+	// listen for clear button click
+})
